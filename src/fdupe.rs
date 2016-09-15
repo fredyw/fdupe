f940a80cd5f55d78bb864a8102c86ad2d742ba94
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-09-15 01:23:35 -0500

    Add some walkdir

--- src/fdupe.rs
+++ src/fdupe.rs
@@ -1,7 +1,10 @@
-use std::cmp;
-
+extern crate walkdir;
 extern crate unicode_segmentation;
+
+use std::cmp;
+use std::path::Path;
 use self::unicode_segmentation::UnicodeSegmentation;
+use self::walkdir::WalkDir;
 
 fn edit_distance(str1: &str, str2: &str) -> usize {
     let str1_vec = UnicodeSegmentation::graphemes(str1, true).collect::<Vec<&str>>();
@@ -28,6 +31,14 @@ fn edit_distance(str1: &str, str2: &str) -> usize {
     dp[str1_size - 1 as usize][str2_size - 1 as usize]
 }
 
+fn get_paths<'a>(dir: &str) {
+    let walker = WalkDir::new(dir).into_iter();
+    for entry in walker.filter_map(|e| e.ok()) {
+        let path = entry.path();
+        println!("{:?}", path.file_name())
+    }
+}
+
 #[cfg(test)]
 mod test {
     use fdupe;
@@ -39,5 +50,10 @@ mod test {
         assert_eq!(3, fdupe::edit_distance("abc", "def"));
         assert_eq!(1, fdupe::edit_distance("忠犬ハチ公", "忠犬ハチ"));
         assert_eq!(2, fdupe::edit_distance("犬ハチ公", "忠犬ハチ"));
+    }
+
+    #[test]
+    fn test_get_paths() {
+        fdupe::get_paths("/tmp");
     }
 }
