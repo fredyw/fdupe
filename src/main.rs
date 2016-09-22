c3afc824ac8bac1143cae36e14817eee562ecfd1
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-09-22 01:14:33 -0500

    Better error messages

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,10 @@ fn err_and_exit(message: &str) {
     process::exit(1);
 }
 
+fn err_req_opt(opt: &str) {
+    err_and_exit(&format!("{} is a required option", opt));
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let program = args[0].clone();
@@ -31,7 +35,10 @@ fn main() {
     }
     let matches = match opts.parse(&args[1..]) {
         Ok(m) => { m }
-        Err(f) => { panic!(f.to_string()) }
+        Err(f) => {
+            err_and_exit(&f.to_string());
+            return
+        }
     };
     if matches.opt_present("h") {
         print_usage(&program, opts);
@@ -42,25 +49,36 @@ fn main() {
             {
                 let path = Path::new(&d);
                 if !path.is_dir() {
-                    panic!("{} is not a directory", d)
+                    err_and_exit(&format!("{} is not a directory", d));
+                    return
                 }
             }
             d
         }
-        None => { panic!("-d is a required option") }
+        None => {
+            err_req_opt("-d is a required option");
+            return
+        }
     };
     let n_ed = match matches.opt_str("n") {
         Some(n) => {
             let n = match n.parse::<i32>() {
                 Ok(n) => { n }
-                Err(f) => { panic!(f.to_string()) }
+                Err(f) => {
+                    err_and_exit(&f.to_string());
+                    return
+                }
             };
             if n < 0 {
-                panic!("number of edit distance must be >= 0, got {}", n);
+                err_and_exit(&format!("number of edit distance must be >= 0, got {}", n));
+                return
             }
             n
         }
-        None => { panic!("-n is a required option") }
+        None => {
+            err_req_opt("-n is a required option");
+            return
+        }
     };
     let filter = matches.opt_str("t");
     fdupe::fdupe::find_duplicates(&dir, n_ed, filter);
